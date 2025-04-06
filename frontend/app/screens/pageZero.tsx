444d558391c27361473003404b2114eadca262b2
Bipana Bastola <bastolabipana006@gmail.com>
2025-04-06 15:08:30 +0400

    dont know

--- frontend/app/screens/pageZero.tsx
+++ frontend/app/screens/pageZero.tsx
@@ -79,7 +79,7 @@ export default function PageZero() {
 
 
       // Check if the user exists in the database
-      const dbResponse = await fetch("http://127.0.0.1:8000/api/users/check", {
+      const dbResponse = await fetch("https://campuscraves.onrender.com/api/users/check", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ googleId }),
